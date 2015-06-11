e990a1fd62a4e29cc99ce51755b78e6cf33a1b92
Anton Lundin <anton.lundin@softhouse.se>
2015-06-11 15:33:42 +0200

    updated the test messages and made the checks slightly more thorough

--- test/test.js
+++ test/test.js
@@ -6,6 +6,8 @@ var request2 = require('supertest');
 var expect = require('expect.js');
 var url = 'localhost:9000';
 
+//npm install mocha, superagent, supertest, expect.js
+
 describe('server', function () {
     before(function (done) {
         done();
@@ -17,11 +19,12 @@ describe('server', function () {
     // });
 });
 
-describe('Testing attributes:', function () {
+describe('/api/attribute', function () {
     var id;
 
    //getting all attributes
-    it('Getting all attributes', function(done) {
+    it('should reply with HTTP status code 200 when the correct email and ' + 
+        'user headers are set', function(done) {
         request2(url)
             .get('/api/attribute')
             .set('x-forwarded-email', 'anton.lundin2@softhouse.se')
@@ -40,7 +43,7 @@ describe('Testing attributes:', function () {
     });
 
     //posting an attribute
-    it('Posting an attribute test1', function(done) {
+    it('should respond with a correctly formatted json object containing the posted attribute "test1"', function(done) {
         request2(url)
             .post('/api/attribute')
             .set('x-forwarded-email', 'anton.lundin2@softhouse.se')
@@ -56,13 +59,23 @@ describe('Testing attributes:', function () {
                 }
                 expect(res).to.exist;
                 expect(res.status).to.equal(200);
+                expect(Object.keys(res.body).length).to.equal(4);
+
+                expect(res.body).to.have.key('createdAt');
+                expect(res.body).to.have.key('updatedAt');
+                expect(res.body).to.have.key('_id');
+                expect(res.body).to.have.key('name');
+
+                expect(res.body.name).to.equal('test1');
+                expect("0x" + res.body._id).to.be.within(0, Infinity);
+
                 id = res.body._id;
                 done();
             });
     });
 
     //retrieving previously posted attribute by its id
-    it('Retrieving attribute test1', function(done) {
+    it('should respond with a correctly formatted json object containing the previously posted attribute "test1"', function(done) {
         console.log('/api/attribute/' + id);
         request2(url)
             .get('/api/attribute/' + id)
@@ -77,12 +90,23 @@ describe('Testing attributes:', function () {
                 }
                 expect(res).to.exist;
                 expect(res.status).to.equal(200);
+                expect(Object.keys(res.body).length).to.equal(4);
+
+                expect(res.body).to.have.key('createdAt');
+                expect(res.body).to.have.key('updatedAt');
+                expect(res.body).to.have.key('_id');
+                expect(res.body).to.have.key('name');
+
+                expect(res.body.name).to.equal('test1');
+                expect("0x" + res.body._id).to.be.within(0, Infinity);
+                expect(res.body._id).to.equal(id);
+
                 done();
             });
     });
 
     //delete previously posted attribute by its ids
-    it('Deleting attribute test1', function(done) {
+    it('should respond with HTTP status code 200 after deleting attribute "test1"', function(done) {
         console.log('/api/attribute/' + id);
         request2(url)
             .delete('/api/attribute/' + id)
@@ -97,6 +121,8 @@ describe('Testing attributes:', function () {
                 }
                 expect(res).to.exist;
                 expect(res.status).to.equal(200);
+                expect(res.body).to.have.key('message');
+                expect(res.body.key).to.equal('The attribute was successfully deleted');
                 done();
             });
     });    
