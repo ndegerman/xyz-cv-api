a12bc106f0ec5911bc22e6718ea37389eed2cc7d
Rasmus Letterkrantz <Rasmus.Letterkrantz@gmail.com>
2016-02-24 20:43:42 +0100

    Update enable-ssh.sh

--- enable-ssh.sh
+++ enable-ssh.sh
@@ -1,3 +1,4 @@
 # !/bin/sh
 mkdir -p "$HOME/.ssh"
 echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'cd ./xyz-cv-api/ && sudo bash build.sh'
