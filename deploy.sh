9d4483f092fc0ae1783cc17d6ab6d77291c81584
Rasmus Letterkrantz <rasmus.letterkrantz@gmail.com>
2016-02-24 12:46:39 +0100

    changed the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 # !/bin/sh
 set -e
 git pull
-docker build . -t kushapi
-docker run
+docker kill kushapi
+docker build -t kushapi .
+docker run -d --name kushapi -e KATALOG_VHOSTS=default/cv-api kushapi
 ^D
