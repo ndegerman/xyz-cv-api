49f638ac02079b6614fc418af255b7450af5039b
Rasmus Letterkrantz <Rasmus.Letterkrantz@gmail.com>
2016-02-25 17:50:44 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 # !/bin/sh
 mkdir -p "$HOME/.ssh"
 echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
+chmod 600 $HOME/.ssh/id_rsa
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'cd ./xyz-cv-api/ && sudo bash build.sh'
